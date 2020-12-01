4049792e52c0e85562ed07bcd7dbfe26265cc122
Michal Holiš <holis@argutec.eu>
2020-12-01 15:57:57 +0100

    Initial commit

---
+++ examples/Example.Simple/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using Argutec.Profiling;
+
+namespace Example.Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (new Profiler("MyMethod"))
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ src/Argutec.Profiling/Profiler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace Argutec.Profiling
+{
+    /// <summary>
+    /// Simple profiler. Usage:
+    /// using (new Profiler("MyMethod performance"))
+    /// {
+    ///     MyMethod();    
+    /// }
+    /// </summary>
+    public class Profiler : IDisposable
+    {
+        private string mDescription;
+        private Stopwatch mStopwatch = new Stopwatch();
+
+        /// <summary>
+        /// Starts profiling.
+        /// </summary>
+        /// <param name="aDescription">Description of the profiler that will be printed into log.</param>
+        public Profiler(string aDescription)
+        {
+            mDescription = aDescription;
+            Console.WriteLine($"[..] Task '{mDescription}' started.");
+            mStopwatch.Start();
+        }
+
+        public void Dispose()
+        {
+            mStopwatch.Stop();
+            TimeSpan lElapsedTime = mStopwatch.Elapsed;
+
+            Console.WriteLine($"[OK] Task '{mDescription}' completed in {lElapsedTime.TotalMilliseconds} ms.");
+        }
+    }
+}
